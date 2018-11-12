33237ae6caafc3f83e901607b0da43c89e2af15d
Katerina Arvay <kate@arvay.cz>
2018-11-12 22:08:08 +0100

    redo create react app to simple auth form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: 'swordfish',
+      authorized: false
+    };
+    this.authorize = this.authorize.bind(this);
+  }
+  authorize(e) {
+    const password = e.target.querySelector(
+      'input[type="password"]').value;
+    const auth = (password === this.state.password);
+    this.setState({
+      authorized: auth
+    });
+  }
   render() {
+    const login = (
+      <form action="#" onSubmit={this.authorize}>
+        <input type="password" placeholder="Password" />
+        <input type="submit" />
+      </form>
+     );
+    const contactInfo = (
+      <ul>
+        <li>
+           client@example.com
+        </li>
+         <li>
+            555.555.5555
+          </li>
+       </ul>
+    );
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="authorization">
+        <h1>{this.state.authorized ? 'Contact' : 'Enter the Password'}</h1>
+        {this.state.authorized ? contactInfo : login}
+        <ul>
+          <li>
+            client@example.com
+          </li>
+          <li>
+            555.555.5555
+          </li>
+        </ul>
       </div>
     );
   }
